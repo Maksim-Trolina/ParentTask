2271599f4b88d578ef0a8014918f93e2cd021b68
Maksim.Panasenko <maks.panasenko01@bk.ru>
2021-12-15 22:09:15 +0300

    complete

---
+++ ConsoleApp1/Human.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp1
+{
+    internal class Human
+    {
+        public string Name { get; set; }
+        IEnumerable<Human> children;
+        public IEnumerable<Human> Children
+        {
+            get => children;
+            set
+            {
+                if (value is not null && value.Count() > 2)
+                    throw new ArgumentOutOfRangeException("Maximum two children");
+                children = value;
+            }
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,8 +1,35 @@
-﻿int A()
+﻿using ConsoleApp1;
+
+try
 {
-    return 1;
+    Human human = new Human() { Name = "Bob", Children = new Human[] {  new Human { Name = "Dima", Children = new Human[] { new Human { Name = "Ivan" } } }, new Human { Name = "Lol" } } };
+    GetChildren(human);
+}
+catch(ArgumentOutOfRangeException e)
+{
+    Console.WriteLine(e.Message);
 }
 
-Console.WriteLine("Hello, World!");
 
-Console.WriteLine(A());
+void GetChildren(Human parent)
+{
+    if (parent is null)
+        return;
+    Console.WriteLine($"Parent name: {parent.Name}");
+    DisplayChildrenNames(parent);
+
+}
+
+void DisplayChildrenNames(Human parent)
+{
+    if (parent.Children is null)
+        return;
+    foreach (var child in parent.Children)
+    {
+        if (child is not null)
+        {
+            Console.WriteLine($"Child name: {child.Name}");
+            DisplayChildrenNames(child);
+        }
+    }
+}
